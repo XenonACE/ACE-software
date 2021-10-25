05f8b2a325c7c83553f10c62079a157800136ce4
Vince R <r2d2fan20@outlook.com>
2021-10-25 18:27:44 -0500

    ha

--- NewApplications/Settings/main.c
+++ NewApplications/Settings/main.c
@@ -3,12 +3,24 @@
 
 #define FONT "-misc-montserrat-bold-r-normal--0-0-0-0-p-0-ascii-0"
 
+int scrolloffset = 0;
+
+void hideWindowBtnCallback(hWidget *w, hWidget *scr) {
+	printf("Hello, World!\n");
+}
+
+void recalculateScroll(hWidget *w, hWidget *scr) {
+	printf("%f\n", scr->pos);
+}
+
 int main() {
 #include "icon.xpm"
-	hWindow *win = createWindow(0, 0, 405, 720, "Settings", 0x222222, 0xFFFFFF, 200, 200, 0, icon_xpm, NULL);
+	hWindow *win = createWindow(50, 50, 405, 720, "Settings", 0x222222, 0xFFFFFF, 200, 200, 0, icon_xpm, NULL);
 	addTextCentered(win, 0, 0, -2, 300, "Settings", 0xFFFFFF, FONT);
 
-	addButton(win, 10, 300, 395, 30, "I am button", 0x222222, 0xFFFFFF, 0, 0, 1, FONT, NULL, NULL);
+	hWidget *scr = (hWidget*)addVScroll(win, -10, 0, 10, 720, 0x222222, 0x222222, 0x000000, 0, 0.5, recalculateScroll, NULL);
+
+	addButton(win, 10, 300 - scrolloffset, 300, 30, "I am button", 0x222222, 0xFFFFFF, 0, 0, 1, FONT, hideWindowBtnCallback, scr);
 
 	eventLoop(win, NULL, NULL);
 	destroyWindow(win);
---
+++ test/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <ACE.h>
+
+int main() {
+	printf("%s", getKernelVersion());
+}
